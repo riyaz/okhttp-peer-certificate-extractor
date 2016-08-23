1df7e2440e7cdae1722c6370279a3630b2d6a31c
Riyaz Mohammed Ibrahim <riyaz@users.noreply.github.com>
2016-08-23 22:50:50 +0530

    Fetch the certificates from websites

--- src/main/java/org/fabiomsr/peercertificate/PeerCertificateExtractor.java
+++ src/main/java/org/fabiomsr/peercertificate/PeerCertificateExtractor.java
@@ -1,10 +1,17 @@
 package org.fabiomsr.peercertificate;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
@@ -15,16 +22,53 @@ import java.util.Base64;
  */
 public class PeerCertificateExtractor {
 
+
+
+    public static void main(String[] args) throws IOException, URISyntaxException, NoSuchAlgorithmException {
+        URL[] url = {
+                new URL("https://github.com"),
+        };
+
+        for (URL anUrl : url) {
+            System.out.println(anUrl.toString());
+            String sha = extract(anUrl);
+            System.out.println(sha);
+            System.out.println();
+        }
+
+    }
+
+    public static String extract(URL url) throws IOException, NoSuchAlgorithmException {
+        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        urlConnection.connect();
+        Certificate certificate = urlConnection.getServerCertificates()[0];
+
+        byte[] publicKeyEncoded = certificate.getPublicKey().getEncoded();
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] publicKeySha256 = messageDigest.digest(publicKeyEncoded);
+        byte[] publicKeyShaBase64 = Base64.getEncoder().encode(publicKeySha256);
+
+//        String encoded = new BigInteger(1, publicKeyEncoded).toString(16);
+//        System.out.println(url.toString());
+        String out = "sha256/" + new String(publicKeyShaBase64);
+//        System.out.println(out);
+//        System.out.println(encoded);
+//        System.out.println("\n");
+        return out;
+    }
+
     /**
      * Get peer certificate(Public key to sha256 to base64)
+     *
      * @param certificate Crt or der or pem file with a valid certificate
      * @return
      */
-    public static String extract(File certificate){
+
+    public static String extract(File certificate) {
 
         FileInputStream inputStream = null;
 
-        try{
+        try {
             inputStream = new FileInputStream(certificate);
             X509Certificate x509Certificate = (X509Certificate) CertificateFactory.getInstance("X509")
                     .generateCertificate(inputStream);
@@ -34,8 +78,8 @@ public class PeerCertificateExtractor {
             byte[] publicKeySha256 = messageDigest.digest(publicKeyEncoded);
             byte[] publicKeyShaBase64 = Base64.getEncoder().encode(publicKeySha256);
 
-           return "sha256/" + new String(publicKeyShaBase64);
-        }catch (Exception e){
+            return "sha256/" + new String(publicKeyShaBase64);
+        } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
